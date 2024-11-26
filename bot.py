ea485ba8ed4903e9fa199f53e33274fb5b6bdb41
Илья Зоренко <105580858+dobrozor@users.noreply.github.com>
2024-11-26 12:22:49 +0300

    Update bot.py
    Обновление 100% работоспособность

--- bot.py
+++ bot.py
@@ -1,136 +1,111 @@
-import re
-
 from bs4 import BeautifulSoup as bs
 import time
 from selenium import webdriver
 import telebot
-import datetime
+from telebot import types
 
-# Настройки
-myLogin = ''  # логин от профи.ру
-myPassword = ''  # парль
-main_key = {'Дизайнеры',
-            'Дизайн', 'Разработка логотипа', 'Графический дизайн', 'инфографика', 'инфографики', 'нарисовать', 'создать', 'дизайнер', 'создание', 'редизайн', 'разработка', 'обработка', 'макет', ''} #
-time_key = {'часов',
-            'час', 'Вчера', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентрября', 'ноября', 'октября', 'декабря'}# Простейшее ограничение по времени
-bad_words = {'Опрос', '3D', 'копирайтер', 'копирайт', 'моушн', 'видео', 'анимация'}
+# Settings
+myLogin = ''  # логин (телефон не катит)
+myPassword = ''  # пароль от профи.ру
 
 token = '' #токен бота
-chat_id = '' #айди группы кудда бот отправит сообщение начинается с -100.....
+chat_id = '' #сюда айди группы с ботом (создаёте беседу, в неё добавляете бота и вот йади беседы нужен, он всегда начинается с -100)
 
+time_key = {'часов', 'час', 'Вчера', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентрября', 'ноября', 'октября', 'декабря'}# Простейшее ограничение по времени
+bad_words = {'Опрос', '3D', 'копирайтер', 'копирайт', 'моушн', 'видео', 'анимация', '3d', 'Опросы'}
+
+#=========== ОСТАЛЬНОЕ ЛУЧШЕ НЕ МЕНЯТЬ ЕСЛИ НЕ ШАРИШ В ПИТОНЕ ============
 url = 'https://profi.ru/backoffice/n.php'
-url_site = 'https://rnd.profi.ru'
-
-
-def refreshPage():
-    driver.refresh()
-    time.sleep(5)
-    return
-
+sent_links = set()  # Set to keep track of sent messages
 
 bot = telebot.TeleBot(token)
 options = webdriver.ChromeOptions()
 options.add_argument(
     "user-agent=Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)")
+driver = webdriver.Chrome(options=options)
 
-driver = webdriver.Chrome(
-    options=options
-)
+def refresh_page():
+    driver.refresh()
+    time.sleep(10)
 
-# Логинемся
+def send_message(subject, description, price, time_info, link):
+    message = f"<b>{subject}</b>\n"
+    if price:
+        message += f"<b>{price}</b>\n"
+    message += f"\n{description}\n\n<i>{time_info}</i>"
+    markup = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton("Откликнуться", url=f"https://rnd.profi.ru{link}")
+    markup.add(button)
+    bot.send_message(chat_id, message, reply_markup=markup, parse_mode='HTML')
+
+def check_conditions(subject):
+    if any(bad_word in subject for bad_word in bad_words):
+        return False
+    return True
+
+# Авторизация на сайте
 driver.get(url)
+
+# Ввод логина и пароля
 namesLogin = driver.execute_script(
     "return document.getElementsByClassName('ui-input ui-input-bo login-form__input-login ui-input_desktop ui-input_with-placeholder_empty');")
 namesLogin[0].clear()
-email_input = namesLogin[0].send_keys(myLogin)
-
-time.sleep(0.5)
+namesLogin[0].send_keys(myLogin)
 namesPassword = driver.execute_script(
     "return document.getElementsByClassName('ui-input ui-input-bo login-form__input-password ui-input_desktop ui-input_with-placeholder_empty');")
 namesPassword[0].clear()
-password_input = namesPassword[0].send_keys(myPassword)
-time.sleep(0.5)
+namesPassword[0].send_keys(myPassword)
+namesButton = driver.execute_script("return document.getElementsByClassName('ui-button');")
+namesButton[0].click()
 
-namesButton = driver.execute_script(
-    "return document.getElementsByClassName('ui-button');")
-button_input = namesButton[0].click()
+# Пауза для ввода капчи
+print("Пожалуйста, введите капчу...")
+time.sleep(30)
 
-# Время для ввода капчи
-time.sleep(60)
+while True:
+    print("Обновление страницы для получения данных...")
+    # Обновляем страницу и получаем новые данные
+    refresh_page()
+    page = driver.page_source
+    soup = bs(page, "html.parser")
+    containers = soup.find_all(class_="OrderSnippetContainerStyles__Container-sc-1qf4h1o-0")  # Измените класс здесь
 
-page = driver.page_source
+    if not containers:
+        print("Ошибка в HTML, напиши в тг: @dobrozor")
+        continue
 
-soup = bs(page, 'html.parser')
-known_tasks = [] #В целом уже не нужно, требовалось для старой проверки
+    for container in containers:
+        link_element = container.find('a', class_="SnippetBodyStyles__Container-sc-tnih0-2")
+        subject_element = container.find(class_="SubjectAndPriceStyles__SubjectsText-sc-18v5hu8-1")
+        description_element = container.find(class_="SnippetBodyStyles__MainInfo-sc-tnih0-6")
+        price_element = container.find(class_="SubjectAndPriceStyles__PriceValue-sc-18v5hu8-5")
+        time_element = container.find(class_="Date__DateText-sc-e1f8oi-1")
 
-try:
-    sent_links = []  # Список для отслеживания отправленных ссылок
+        if link_element and subject_element and description_element:
+            subject = subject_element.text.strip()
+            description = description_element.text.strip()
+            price = price_element.text.strip() if price_element else None
+            time_info = time_element.text.strip()
+            link = link_element['href']
 
-    while True:
-        refreshPage()
-        print('обновил страницу')
+            print(f"Получены данные: '{subject}'")
+            # Проверка на совпадение времени
+            if any(time_word in time_info for time_word in time_key):
+                print(f"Сообщение не отправлено: '{time_info}' содержит фильтр.")
+                continue
+            # Проверка на наличие 'плохих' слов
+            if any(bad_word in subject for bad_word in bad_words):
+                print(f"Сообщение не отправлено: '{subject}' содержит фильтр.")
+                continue
 
-        arr = driver.execute_script(
-            "return document.getElementsByClassName('SnippetBodyStyles__Container-sc-br3c4b-0');")
-        print('получил данные')
-
-        for block in soup.find_all(class_=re.compile('SnippetBodyStyles__Container-')):
-            name = block.find(class_=re.compile('SubjectAndPriceStyles__SubjectsText-'))
-            about = block.find(class_=re.compile('SnippetBodyStyles__MainInfo-'))
-            price = block.find(class_=re.compile('SubjectAndPriceStyles__PriceLine-'))
-            date = block.find(class_=re.compile('Date__DateText-'))
-            my_url = url_site + str(block.attrs['href'])
-
-            # Создаем задачу для проверки
-            task_stack = (str(about.get_text()), str(date.get_text()), my_url)
-
-            tg_name = str(name.get_text())
-            tg_about = str(about.get_text())
-            tg_price = str(price.get_text())
-            tg_date = str(date.get_text())
-
-            # Проверка на наличие хотя бы одного слова из main_key в task_stack
-            contains_keyword = any(key.lower() in i.lower() for key in main_key for i in task_stack)
-
-            # Проверка, что нет слов из time_key
-            not_in_time_key = all(key2.lower() not in str(task_stack) for key2 in time_key)
-
-            # Проверка на отсутствие bad_words
-            not_contains_bad_word = all(bad_word.lower() not in str(task_stack) for bad_word in bad_words)
-
-            # Основная проверка на выполнение условий
-            if contains_keyword and not_in_time_key and not_contains_bad_word:
-                print('Нашел подходящее')
-
-                # Проверяем, отправлял ли бот нам новый заказ или нет
-                if my_url not in sent_links:
-                    sent_links.append(my_url)  # если не отправлял то добавляем в отправленные и отправляем заказ в тг
-
-
-                    message_text = f"*{tg_name}*\n{tg_price}\n\n{tg_about}\n\n_{tg_date}_"
-
-                    # Создаем клавиатуру и кнопку
-                    keyboard = telebot.types.InlineKeyboardMarkup()
-                    callback_button = telebot.types.InlineKeyboardButton("Написать", url=my_url)
-                    keyboard.add(callback_button)
-
-                    # Отправляем сообщение с кнопкой
-                    bot.send_message(chat_id, message_text, reply_markup=keyboard, parse_mode='Markdown')
-                    current_time = datetime.datetime.now().time()
-                    print(f'Отправил в тг в {current_time}')
-                else:
-                    print('А нет, уже такое отправлял')
+            if description not in sent_links and check_conditions(subject):
+                print(f"Проходит проверку: '{subject}'")
+                send_message(subject, description, price, time_info, link)
+                sent_links.add(description)  # Добавляем ссылку в список отправленных сообщений
+                print(f"Сообщение отправлено.")
             else:
-                print('Ничего нового :(')
+                print(
+                    f"Данные не прошли проверку: '{subject}'")
 
-        time.sleep(90)
-        driver.get(url)
-        page = driver.page_source
-        soup = bs(page, 'html.parser')
-
-except Exception as ex:
-    print(ex)
-
-finally:
-    driver.close()
-    driver.quit()
+    print("Ожидание перед следующим обновлением...")
+    time.sleep(60)
